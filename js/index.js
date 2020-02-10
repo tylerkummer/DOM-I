7113c7abf4371a9f4a3017d7617d9144ea020045
Tyler Kummer <tylerkummer@protonmail.com>
2020-02-10 15:06:22 -0600

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -39,4 +39,43 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const navBar = document.querySelectorAll('a');
+navBar[0].textContent = "Services";
+navBar[1].textContent = "Product";
+navBar[2].textContent = "Vision";
+navBar[3].textContent = "Features";
+navBar[4].textContent = "About";
+navBar[5].textContent = "Contact";
+
+const header1 = document.querySelector('h1');
+header1.innerHTML = `Dom<br/>Is<br/>Awesome`
+
+const btn = document.querySelector('button');
+btn.textContent = 'Get Started'
+
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.src = "img/header-img.png";
+
+const middleImg = document.querySelector("#middle-img");
+middleImg.src = "img/mid-page-accent.jpg";
+
+const titles = document.querySelectorAll('h4');
+titles[0].textContent = "Features";
+titles[1].textContent = "About";
+titles[2].textContent = "Services";
+titles[3].textContent = "Product";
+titles[4].textContent = "Vision";
+titles[5].textContent = "Contact";
+
+const filler = document.querySelectorAll('p');
+filler[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+filler[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+filler[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+filler[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+filler[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+filler[5].innerHTML = "123 Way 456 Street<br/>Somewhere, USA";
+filler[6].textContent = "1 (888) 888-8888";
+filler[7].textContent = "sales@greatidea.io";
+filler[8].textContent = "Copyright Great Idea! 2018";
